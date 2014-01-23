8e7a1fcd3b21ae2c9e7a9277da0223f51f39c62c
Jillberth Estillore <jestillore@zoogtech.com>
2014-01-23 10:38:55 +0800

    Initialize the repository.

---
+++ review-centre.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The WordPress Plugin Boilerplate.
+ *
+ * A foundation off of which to build well-documented WordPress plugins that
+ * also follow WordPress Coding Standards and PHP best practices.
+ *
+ * @package   Review Centre
+ * @author    Jillberth Estillore <contact@jillberthestillore.com>
+ * @license   GPL-2.0+
+ * @link      http://www.jillberthestillore.com
+ * @copyright 2014 Jillberth Estillore
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Review Centre
+ * Plugin URI:        http://downloads.jillberthestillore.com/wordpress/plugins/review-centre
+ * Description:       Review management system for wordpress websites.
+ * Version:           1.0.0
+ * Author:            Jillberth Estillore
+ * Author URI:        http://www.jillberthestillore.com
+ * Text Domain:       plugin-name-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/jestillore/review-centre
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-review-centre.php' );
+
+register_activation_hook( __FILE__, array( 'Review_Centre', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Review_Centre', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'Review_Centre', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-review-centre-admin.php' );
+	add_action( 'plugins_loaded', array( 'Review_Centre_Admin', 'get_instance' ) );
+
+}
---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package   Review_Centre
+ * @author    Jillberth Estillore <contact@jillberthestillore.com>
+ * @license   GPL-2.0+
+ * @link      http://www.jillberthestillore.com
+ * @copyright 2014 Jillberth Estillore
+ */
+
+// If uninstall not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// @TODO: Define uninstall functionality here
